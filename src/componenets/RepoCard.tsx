59373988f8113fe8a2cee5ee77d1094b78d53f76
Alex <Almarkiz777@gmail.com>
2024-09-04 15:25:33 +0300

    final

--- src/componenets/RepoCard.tsx
+++ src/componenets/RepoCard.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IRepo} from "../models/models";
+import {useActions} from "../hooks/actions";
+import {useAppSelector} from "../hooks/redux";
 
 export const RepoCard = ({repo}: { repo: IRepo }) => {
+
+    const {addFavourite, removeFavourite} = useActions();
+    const {favourites} = useAppSelector(state => state.github);
+    const [isFavourite, setIsFavourite] = useState(favourites.includes(repo.html_url));
+
+    const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        addFavourite(repo.html_url)
+        setIsFavourite(true);
+    }
+
+    const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        removeFavourite(repo.html_url)
+        setIsFavourite(false);
+    }
+
     return (
         <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-200 transition-all">
             <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
@@ -13,6 +32,14 @@ export const RepoCard = ({repo}: { repo: IRepo }) => {
                 <p className="text-sm font-thin">
                     {repo?.description}
                 </p>
+                {!isFavourite && <button className="py-2 px-3 bg-amber-400 rounded mr-2 hover:shadow-md transition-all"
+                        onClick={addToFavourite}
+                >Add
+                </button>}
+                {isFavourite && <button className="py-2 px-3 bg-red-400 rounded hover:shadow-md transition-all"
+                        onClick={removeFromFavourite}
+                >Remove
+                </button>}
             </a>
         </div>
     );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,18 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {gitHubApi} from "./githib/github.api";
 import {setupListeners} from "@reduxjs/toolkit/query";
+import {githubReducer} from "./githib/github.slice";
 
 
 export const store = configureStore({
     reducer: {
-        [gitHubApi.reducerPath]: gitHubApi.reducer
+        [gitHubApi.reducerPath]: gitHubApi.reducer,
+        github: githubReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(gitHubApi.middleware)
 })
 
 setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
